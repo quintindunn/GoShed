3cbac853babdada0b844b05695cfc6a23f32030b
Quintin Dunn <quintindunn67@gmail.com>
2025-06-04 13:57:11 -0400

    Added home page

--- Web/controllers/home.go
+++ Web/controllers/home.go
@@ -1,7 +1,30 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"com.quintindev/WebShed/database"
+	"com.quintindev/WebShed/hardwareInterface"
+	"com.quintindev/WebShed/models"
+	"com.quintindev/WebShed/utils"
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
-func Home(c *gin.Context) {
-	c.JSON(200, gin.H{})
+func Page(c *gin.Context) {
+	hardwareInterface.GetExpiredCodes()
+
+	var ModelRollingCodes []models.RollingCode
+	database.DB.Find(&ModelRollingCodes, "nullified = ?", false)
+
+	var RollingCodes []gin.H
+	for _, code := range ModelRollingCodes {
+		RollingCodes = append(RollingCodes, gin.H{
+			"code":   code.Code,
+			"expiry": time.Unix(code.Expiry, 0).Format("01-02-06 3:04 PM"),
+		})
+	}
+	data := gin.H{
+		"lockLocked":   hardwareInterface.GetGetLocked(),
+		"rollingCodes": RollingCodes,
+	}
+	utils.Render(c, 200, "home", data)
 }
